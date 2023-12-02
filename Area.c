b2a2660200ac95c901de4d73fa502ead570fa97d
Sneha0199lakhan <152689025+Sneha0199lakhan@users.noreply.github.com>
2023-12-02 14:51:58 +0530

    Add files via upload

---
+++ Area.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    float Radius=0.0f;
+    const float PI= 3.14f;
+    float Area= 0.0f;
+
+    printf("Enter the Size of radius: \n");
+    scanf("%f",&Radius);
+
+    Area= PI * Radius * Radius ;
+
+
+    printf (" Area of Circle is : %f\n", Area);
+
+return 0;
+
+
+
+}
